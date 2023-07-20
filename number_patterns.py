dcb450006b4a68f15cf3bb9e539555abf1885bb2
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2023-07-20 16:34:03 -0700

    adding hangman.py game

---
+++ number_patterns.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 27 23:27:26 2023
+
+@author: tcran
+
+right triangle number pattern with specified rows 
+"""
+
+rows = 5
+
+for i in range(rows+1): "python indexing starts at 0"
+    for j range(i):
+        print(i, end=' ')
+    
+    print(' ')
+    
