95cfa5ea111ffc8ac96ff9524830b502e1dfdcd2
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2024-05-05 17:52:41 -0700

    Turtle coordinate system, Snake Game - Added class snake for moving functionailtiy and creating snake object in main

--- understanding-Turtle-coordinatesys.py
+++ understanding-Turtle-coordinatesys.py
@@ -4,21 +4,22 @@ import random
 
 
 screen = Screen()
-screen.setup(width=500, height=400)
+screen.setup(width=1000, height=800)
 is_race_on = False
 
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter the color: ")
-print(f" Your bet is the {user_bet} turtle.")
+print(f"Your bet is the {user_bet} turtle.")
 
 colors = ["red","orange", "yellow", "green", "blue", "purple"]
-y_positions = [-60, -30, 0, 30, 60, 90]
+y_positions = [-60, -20, 20, 60, 100, 140]
 all_turtles = []
 
 for turtle_index in range(0,6):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
+    new_turtle.shapesize(2)
     new_turtle.color(colors[turtle_index])
-    new_turtle.goto(x=-230, y=y_positions[turtle_index])
+    new_turtle.goto(x=-460, y=y_positions[turtle_index])
     all_turtles.append(new_turtle)
 
 if user_bet:
@@ -26,7 +27,7 @@ if user_bet:
 
 while is_race_on is True:
     for turtle in all_turtles:
-        if turtle.xcor() > 230:
+        if turtle.xcor() > 460:
             is_race_on = False
             winning_color = turtle.pencolor()
             if winning_color == user_bet:
