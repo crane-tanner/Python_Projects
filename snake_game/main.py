a550296a24715c5a958c212650cb6012d42ff7c3
Tanner Crane <55415301+Cybernetic1001@users.noreply.github.com>
2024-05-12 14:07:08 -0700

    used class inheritance for food, scoreboard files, added slicing feature, fully functionally snake game.

--- snake_game/main.py
+++ snake_game/main.py
@@ -1,6 +1,8 @@
 from turtle import Screen
 import time
 from snake import Snake
+from food import Food
+from scoreboard import Scoreboard
 
 
 screen = Screen()
@@ -10,6 +12,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -23,6 +27,23 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
 
+    # detect collision with walls
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    #detect collision with tail
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
+    #if head collides with any segment in tail:
+        #gameover
 
 screen.exitonclick()
